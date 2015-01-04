1b5d8fea30a1503cb8d2adb73d2867e6e00eea36
AtChina <weichangying@msn.com>
2015-01-04 21:26:10 +0800

    updte

--- assets/javascript/nav.js
+++ assets/javascript/nav.js
@@ -3,8 +3,19 @@
  * @param  {[type]} ){} [description]
  * @return {[type]}       [description]
  */
-$(function() {
-	if (document.location.location.href === "http://atchina.me/") {
-		document.location.href = "http://atchina.me/about.html";
-	}
-});
+// $(function() {
+// 	var href=document.location.href;
+// 	if (href=== "http://atchina.me/"||href==="http://127.0.0.1:4000/") {
+// 		href= "http://atchina.me/about.html";
+// 	}
+// });
+// window.onload = function() {
+// 	var href = document.location.href;
+// 	if (href === "http://atchina.me/" || href === "http://127.0.0.1:4000/") {
+// 		document.location.href = href + "about.html";
+// 	}
+// };
+var href = document.location.href;
+if (document.location.href === "http://atchina.me/" || document.location.href === "http://127.0.0.1:4000/") {
+	document.location.href = href + "about.html";
+}
