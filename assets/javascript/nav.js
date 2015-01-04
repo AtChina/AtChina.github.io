5e7853fdbf1801e0b533a8b2cf8084f62689861f
AtChina <weichangying@msn.com>
2015-01-04 09:48:54 +0800

    'update'

---
+++ assets/javascript/nav.js
@@ -0,0 +1,10 @@
+/**
+ * 单页面处理程序
+ * @param  {[type]} ){} [description]
+ * @return {[type]}       [description]
+ */
+$(function() {
+	if (document.location.location.href === "http://atchina.me/") {
+		document.location.href = "http://atchina.me/about.html";
+	}
+});
