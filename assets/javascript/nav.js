29bbdc9b4a7421d1ca1e041529c5a058a371fc6a
AtChina <weichangying@msn.com>
2015-01-04 21:30:23 +0800

    Redirect Index.html Page To About.html Page,when first Visit Blog

--- assets/javascript/nav.js
+++ assets/javascript/nav.js
@@ -1,20 +1,4 @@
-/**
- * 单页面处理程序
- * @param  {[type]} ){} [description]
- * @return {[type]}       [description]
- */
-// $(function() {
-// 	var href=document.location.href;
-// 	if (href=== "http://atchina.me/"||href==="http://127.0.0.1:4000/") {
-// 		href= "http://atchina.me/about.html";
-// 	}
-// });
-// window.onload = function() {
-// 	var href = document.location.href;
-// 	if (href === "http://atchina.me/" || href === "http://127.0.0.1:4000/") {
-// 		document.location.href = href + "about.html";
-// 	}
-// };
+//Redirect Index.html Page To About.html Page,when first Visit Blog.
 var href = document.location.href;
 if (document.location.href === "http://atchina.me/" || document.location.href === "http://127.0.0.1:4000/") {
 	document.location.href = href + "about.html";
